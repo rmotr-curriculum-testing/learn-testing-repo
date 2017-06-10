b6b1c5b3f1a4155c255e7fe9165eb8b8cd7775bb
Santiago Basulto <santiago.basulto@gmail.com>
2017-06-10 18:45:15 -0300

    Create test_calculator_with_multiple_operations.py

---
+++ unit-3-mixed-reading-and-assignment-lessons/lesson-4-assignment-multiple-code-blocks/tests/test_calculator_with_multiple_operations.py
@@ -0,0 +1,12 @@
+def test_calculator_with_multiple_operations():
+    calc = Calculator(
+        operations={
+            'add': AddOperation,
+            'subtract': SubtractOperation
+        }
+    )
+    res = calc.calculate(1, 5, 13, 2, 'add')
+    assert res == 21
+    
+    res = calc.calculate(13, 3, 7, 'subtract')
+    assert res == 3
