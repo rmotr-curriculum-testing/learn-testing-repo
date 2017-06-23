0ac92cb65dfd637320e4c01564e71a1126742184
Santiago Basulto <santiago.basulto@gmail.com>
2017-06-23 15:08:37 -0300

    Update test_calculator_with_multiple_operations.py

--- unit-3-mixed-reading-and-assignment-lessons/lesson-4-assignment-multiple-code-blocks/tests/test_calculator_with_multiple_operations.py
+++ unit-3-mixed-reading-and-assignment-lessons/lesson-4-assignment-multiple-code-blocks/tests/test_calculator_with_multiple_operations.py
@@ -7,6 +7,6 @@ def test_calculator_with_multiple_operations():
     )
     res = calc.calculate(1, 5, 13, 2, 'add')
     assert res == 21
-    
+
     res = calc.calculate(13, 3, 7, 'subtract')
     assert res == 3
